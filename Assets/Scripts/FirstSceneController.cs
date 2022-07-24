52bc0fc36e0985f03117b3ae91f49ac4adcb7b36
Book Sadprasid <booksadprasid@gmail.com>
2022-07-24 13:51:57 -0300

    Create a follow menu

---
+++ Assets/Scripts/FirstSceneController.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class FirstSceneController : MonoBehaviour {
+  public void OnRecordButtonPress() {
+    Debug.Log("Record Button Pressed");
+  }
+
+  public void OnReplayButtonPress() {
+    Debug.Log("Replay Button Pressed");
+  }
+}
