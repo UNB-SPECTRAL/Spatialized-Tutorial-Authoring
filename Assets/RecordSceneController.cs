cc1a5678cf58be21c8835fdc9e6b03c4f0f824ef
Book Sadprasid <booksadprasid@gmail.com>
2022-07-28 20:51:41 -0300

    feat: add tooltip prefabs to the instantiated game objects.

--- Assets/RecordSceneController.cs
+++ Assets/RecordSceneController.cs
@@ -1,59 +1,65 @@
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
-using Microsoft.MixedReality.OpenXR;
 using Microsoft.MixedReality.Toolkit;
-using Microsoft.MixedReality.Toolkit.Experimental.Utilities;
 using Microsoft.MixedReality.Toolkit.Input;
 using Microsoft.MixedReality.Toolkit.UI;
-using Microsoft.MixedReality.WorldLocking.Core;
 using Microsoft.MixedReality.WorldLocking.Tools;
 using UnityEngine;
-using Debug = UnityEngine.Debug;
 
 /**
  * This class captures "Air Click"s events and instantiates a game object at that location.
+ *
+ * TODO: Tooltip positions are relative to the starting head pose. This is a problem since
+ * it will load ToolTip that are in front of the user always in front when in World Space
+ * its against a wall. We need to see if we can use the WorlLock managers or context to
+ * help with finding the world position and not the unity position.
  */
 public class RecordSceneController : InputSystemGlobalHandlerListener, IMixedRealityPointerHandler  {
   /***** Public Variables *****/
-  // This sphere will be rendered at the click location.
-  public GameObject sphere;
+  // Represents a prefab which will be rendered at the air click location
+  public GameObject tooltip;
   
   /***** Private Variables *****/
-  private AnchorStore anchorStore;
+  private readonly TooltipStore _tooltipStore;
   
   /***** Constructor *****/
   public RecordSceneController() {
-    anchorStore = GetAnchors();
+    // When instantiating this class, load all the anchors from storage.
+    _tooltipStore = GetTooltips();
   }
 
   #region InputSystemGlobalHandlerListener Implementation
     protected override void RegisterHandlers() {
+      // ReSharper disable once Unity.NoNullPropagation
       MixedRealityToolkit.Instance?.GetService<IMixedRealityInputSystem>()
         ?.RegisterHandler<IMixedRealityPointerHandler>(this);
     }
 
     protected override void UnregisterHandlers() {
+      // ReSharper disable once Unity.NoNullPropagation
       MixedRealityToolkit.Instance?.GetService<IMixedRealityInputSystem>()
         ?.UnregisterHandler<IMixedRealityPointerHandler>(this);
     }
   #endregion InputSystemGlobalHandlerListener Implementation
   
   #region IMixedRealityPointerHandler
+    /** Handles click events. */
     public void OnPointerClicked(MixedRealityPointerEventData eventData) {
-      Debug.Log("Pointer Click");
       
-      // Get the hit points of the AirClick
+      // Get the hit point
       Vector3 hitPoint = eventData.Pointer.Result.Details.Point;
-      // Instantiate a sphere at the hit point.
-      GameObject newSphere = Instantiate(sphere, hitPoint, Quaternion.identity);
+      // Instantiate a tooltip at the hit point.
+      GameObject newTooltip = Instantiate(tooltip, hitPoint, Quaternion.identity);
       // And world lock it.
-      ToggleWorldAnchor twa = newSphere.AddComponent<ToggleWorldAnchor>();
+      ToggleWorldAnchor twa = newTooltip.AddComponent<ToggleWorldAnchor>();
       twa.AlwaysLock = true;
+      
+      // Update the text shown on the tooltip
+      newTooltip.GetComponent<ToolTip>().ToolTipText = "Tooltip #" + (_tooltipStore.tooltipDetailsList.Count + 1);
 
       // And save this sphere for later use
-      SaveAnchor(newSphere);
+      SaveTooltip(newTooltip);
     }
 
     public void OnPointerDown(MixedRealityPointerEventData eventData) {
@@ -70,29 +76,50 @@ public class RecordSceneController : InputSystemGlobalHandlerListener, IMixedRea
   #endregion IMixedRealityPointerHandler
 
   public void LoadAnchors() {
-    // Re-Instantiate spheres from store
-    if(anchorStore.anchors.Count > 0) {
-      foreach(Vector3 anchorPosition in anchorStore.anchors) {
-        Instantiate(sphere, anchorPosition, Quaternion.identity);
+    // Re-Instantiate ToolTip from store
+    if(_tooltipStore.tooltipDetailsList.Count > 0) {
+      foreach(TooltipDetails toolTipDetails in _tooltipStore.tooltipDetailsList) {
+        GameObject newToolTip = Instantiate(tooltip, toolTipDetails.position, Quaternion.identity);
+        newToolTip.GetComponent<ToolTip>().ToolTipText = toolTipDetails.text;
       }
     }
   }
 
-  AnchorStore GetAnchors() {
-    string serializedData = File.ReadAllText(Application.streamingAssetsPath + "/anchors.json");
-    return JsonUtility.FromJson<AnchorStore>(serializedData);
+  TooltipStore GetTooltips() {
+    string serializedData = File.ReadAllText(Application.streamingAssetsPath + "/tooltips.json");
+    return JsonUtility.FromJson<TooltipStore>(serializedData);
   }
   
-  private void SaveAnchor(GameObject anchor) {
-    // Add anchor to anchor store
-    anchorStore.anchors.Add(anchor.transform.position);
-    // Save anchor store
-    string serializedData = JsonUtility.ToJson(anchorStore, true);
-    File.WriteAllText(Application.streamingAssetsPath + "/anchors.json", serializedData);
+  private void SaveTooltip(GameObject toolTip) {
+    // Get tooltip details from the Tooltip prefab
+    string toolTipText = toolTip.GetComponent<ToolTip>().ToolTipText;
+    Vector3 toolTipPosition = toolTip.transform.position;
+    
+    // Add toolTip details to the Tooltip Store
+    TooltipDetails tooltipDetails = new TooltipDetails {
+      text = toolTipText,
+      position = toolTipPosition
+    };
+    _tooltipStore.tooltipDetailsList.Add(tooltipDetails);
+    
+    // Persist the TooltipStore
+    string serializedData = JsonUtility.ToJson(_tooltipStore, true);
+    File.WriteAllText(Application.streamingAssetsPath + "/tooltips.json", serializedData);
   }
   
   [Serializable]
-  public class AnchorStore {
-    public List<Vector3> anchors = new List<Vector3>();
+  public class TooltipStore {
+    public List<TooltipDetails> tooltipDetailsList = new List<TooltipDetails>();
+  }
+  
+  /**
+   * Class which stores tooltip details used to instantiate a Tooltip (
+   *   https://docs.microsoft.com/en-us/windows/mixed-reality/mrtk-unity/mrtk2/features/ux-building-blocks/tooltip?view=mrtkunity-2022-05
+   * ) prefab.
+   */
+  [Serializable]
+  public class TooltipDetails {
+    public string text;
+    public Vector3 position;
   }
 }
